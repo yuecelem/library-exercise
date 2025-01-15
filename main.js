1e821162d5945272c5449be54d3294cee9791321
yuecelem <emre.ycl@hotmail.com>
2025-01-15 15:55:23 +0300

    Add add button that appends to table

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 const table = document.getElementById("table");
+const dialog = document.getElementById('dialog');
+const addBtn = document.getElementById('addBtn');
+const cancelBtn = document.getElementById('cancelBtn');
+const confirmBtn = document.getElementById('confirmBtn');
+
 
 const myLibrary = [];
 
@@ -12,18 +17,76 @@ function Book(title, author, page, publishYear, isRead) {
 
 function addBookToLibrary(title, author, page, publishYear, isRead){
     let newBook = new Book(title, author, page, publishYear, isRead);
+    console.log("New book created:", newBook)
     return myLibrary.push(newBook);
 }
 
 addBookToLibrary('lotr', 'tolkien', 399, 1983, 0)
 addBookToLibrary('dune', 'herbert', 766, 1989, 1)
+addBookToLibrary('book2', 'author2', 7226, 1789, 0)
+
+
 
 console.log(myLibrary)
 
-myLibrary.forEach(book => {
-    let newRow = table.insertRow(-1);
-    for (let i = 0; i < Object.entries(book).length; i++) {
-        let newCell = newRow.insertCell(i);
-        newCell.innerText = book[Object.keys(book)[i]];
+function clearLib() {
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
     }
-});
+}
+
+function makeLib() {
+    console.log(myLibrary)
+    clearLib();
+    
+    myLibrary.forEach(book => {
+        let newRow = table.insertRow(-1);
+        for (let i = 0; i < Object.entries(book).length; i++) {
+            let newCell = newRow.insertCell(i);
+            
+            if(i == 4 && book[Object.keys(book)[i]] === 1) {
+                newCell.innerHTML = '<input type = "checkbox" name = "isRead" checked/>';
+            } else if (i == 4 && book[Object.keys(book)[i]] !== 1) {
+                newCell.innerHTML = '<input type = "checkbox" name = "isRead" unchecked/>';
+            } else newCell.innerText = book[Object.keys(book)[i]];
+        }
+    });
+
+}
+
+makeLib()
+
+
+
+addBtn.addEventListener("click", () => {
+    dialog.showModal();
+})
+
+cancelBtn.addEventListener("click", () => {
+    dialog.close();
+})
+
+confirmBtn.addEventListener("click", (e) => {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const pages = document.getElementById('pages').value;
+    const publishYear = document.getElementById('publish-year').value;
+    const isRead = document.getElementById('is-read').value;
+
+    e.preventDefault();
+
+    if ((title == null || title == '') || (author == null || author == '') || (pages == null || pages == '') || (publishYear == null || publishYear == '') || (isRead == null || isRead == '')) {
+        window.alert("Please fill all fields!")
+        console.log("alert")
+    } else {
+
+    addBookToLibrary(title, author, pages, publishYear, isRead);
+    makeLib();
+    document.querySelector('form').reset();
+    dialog.close(); }
+    
+
+    
+})
+
+
